9d37db593b46f97fdadc71a5d143ea78834768f6
LucasFrazier <lucasfrazier1@gmail.com>
2019-05-17 15:13:21 -0400

    routing working

--- src/containers/LandingPage.js
+++ src/containers/LandingPage.js
@@ -1,10 +1,13 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class LandingPage extends Component {
   render() {
     return (
       <div>
         Here's the Landing Page.
+        <Link to='/login'>Login</Link>
+        <Link to='/register'>Register</Link>
       </div>
     )
   }
--- src/containers/Login.js
+++ src/containers/Login.js
@@ -1,10 +1,12 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Login extends Component {
   render() {
     return (
       <div>
         Here is the Login Page.
+        <Link to='/'>Back to Landing Page</Link>
       </div>
     )
   }
